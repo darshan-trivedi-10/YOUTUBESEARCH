211f53ee530218ad2986b9ab0286c6ae4e90e324
datshan-trivedi-10 <dbtrivedi03@gmail.com>
2022-03-13 13:58:02 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const opn = require('opn');
+let inputArr = process.argv.slice(2);
+console.log(inputArr);
+
+let mainLink = "https://www.youtube.com/results?search_query=";
+
+let nSize = inputArr.length;
+let join = "";
+for (let i = 0; i < nSize; i++) {
+    const element = inputArr[i];
+    if (i == 0) {
+        join = join + element;
+    } else {
+        join = join + "+" + element;
+    }
+}
+
+mainLink = mainLink + join;
+searchInYoutube(mainLink);
+
+function searchInYoutube(url) {
+    opn(url, { app: ['Chrome'] });
+
+}
